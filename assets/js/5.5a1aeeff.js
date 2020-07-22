3705092a25dbe4c8ea3ecfefaffdc81c8a98f222
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-07-22 17:19:58 -0500

    Publishing folder ./docs/.vuepress/dist to gh-pages in 7/22/2020, 5:19:53 PM

---
+++ assets/js/5.5a1aeeff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{727:function(t,n,s){"use strict";s.r(n);var a={props:["photo"]},e=s(70),o=Object(e.a)(a,(function(){var t=this.$createElement;return(this._self._c||t)("a-avatar",{attrs:{src:this.photo}})}),[],!1,null,null,null);n.default=o.exports}}]);
